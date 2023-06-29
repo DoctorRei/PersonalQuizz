83bb83a5f441982a17e7b3748f495abcec70d19d
AkiraRei <Akirarei@yandex.ru>
2023-06-29 21:03:26 +0300

    Initial Commit

---
+++ PersonalQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuizz
+//
+//  Created by Акира on 29.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
